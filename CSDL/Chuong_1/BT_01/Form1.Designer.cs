00927a0201cf4fc432ac3833e315962aa0991ce2
HIEU NGUYEN THANH <thanhhieu961906@gmail.com>
2022-12-27 22:25:09 +0700

    Code CSDL

---
+++ CSDL/Chuong_1/BT_01/Form1.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace BT_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbmasv = new System.Windows.Forms.Label();
+            this.txtmasv = new System.Windows.Forms.TextBox();
+            this.lbhoten = new System.Windows.Forms.Label();
+            this.txthosv = new System.Windows.Forms.TextBox();
+            this.txttensv = new System.Windows.Forms.TextBox();
+            this.ckbphai = new System.Windows.Forms.CheckBox();
+            this.lbphai = new System.Windows.Forms.Label();
+            this.lbngaysinh = new System.Windows.Forms.Label();
+            this.lbnoisinh = new System.Windows.Forms.Label();
+            this.txtngaysinh = new System.Windows.Forms.TextBox();
+            this.txtnoisinh = new System.Windows.Forms.TextBox();
+            this.cmbkhoa = new System.Windows.Forms.ComboBox();
+            this.lbkhoa = new System.Windows.Forms.Label();
+            this.lbhocbong = new System.Windows.Forms.Label();
+            this.lbtongdiem = new System.Windows.Forms.Label();
+            this.txthocbong = new System.Windows.Forms.TextBox();
+            this.txttongdiem = new System.Windows.Forms.TextBox();
+            this.btndau = new System.Windows.Forms.Button();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.txttruoc = new System.Windows.Forms.Button();
+            this.btnhuy = new System.Windows.Forms.Button();
+            this.btnsau = new System.Windows.Forms.Button();
+            this.btnghi = new System.Windows.Forms.Button();
+            this.btncuoi = new System.Windows.Forms.Button();
+            this.btnkhong = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbmasv
+            // 
+            this.lbmasv.AutoSize = true;
+            this.lbmasv.Location = new System.Drawing.Point(27, 25);
+            this.lbmasv.Name = "lbmasv";
+            this.lbmasv.Size = new System.Drawing.Size(63, 21);
+            this.lbmasv.TabIndex = 0;
+            this.lbmasv.Text = "Mã SV";
+            // 
+            // txtmasv
+            // 
+            this.txtmasv.Location = new System.Drawing.Point(145, 18);
+            this.txtmasv.Name = "txtmasv";
+            this.txtmasv.Size = new System.Drawing.Size(315, 28);
+            this.txtmasv.TabIndex = 1;
+            // 
+            // lbhoten
+            // 
+            this.lbhoten.AutoSize = true;
+            this.lbhoten.Location = new System.Drawing.Point(27, 77);
+            this.lbhoten.Name = "lbhoten";
+            this.lbhoten.Size = new System.Drawing.Size(98, 21);
+            this.lbhoten.TabIndex = 0;
+            this.lbhoten.Text = "Họ Tên SV";
+            // 
+            // txthosv
+            // 
+            this.txthosv.Location = new System.Drawing.Point(143, 70);
+            this.txthosv.Name = "txthosv";
+            this.txthosv.Size = new System.Drawing.Size(209, 28);
+            this.txthosv.TabIndex = 1;
+            // 
+            // txttensv
+            // 
+            this.txttensv.Location = new System.Drawing.Point(358, 70);
+            this.txttensv.Name = "txttensv";
+            this.txttensv.Size = new System.Drawing.Size(102, 28);
+            this.txttensv.TabIndex = 1;
+            // 
+            // ckbphai
+            // 
+            this.ckbphai.AutoSize = true;
+            this.ckbphai.Location = new System.Drawing.Point(145, 139);
+            this.ckbphai.Name = "ckbphai";
+            this.ckbphai.Size = new System.Drawing.Size(18, 17);
+            this.ckbphai.TabIndex = 2;
+            this.ckbphai.UseVisualStyleBackColor = true;
+            // 
+            // lbphai
+            // 
+            this.lbphai.AutoSize = true;
+            this.lbphai.Location = new System.Drawing.Point(27, 135);
+            this.lbphai.Name = "lbphai";
+            this.lbphai.Size = new System.Drawing.Size(46, 21);
+            this.lbphai.TabIndex = 0;
+            this.lbphai.Text = "Phái";
+            // 
+            // lbngaysinh
+            // 
+            this.lbngaysinh.AutoSize = true;
+            this.lbngaysinh.Location = new System.Drawing.Point(27, 193);
+            this.lbngaysinh.Name = "lbngaysinh";
+            this.lbngaysinh.Size = new System.Drawing.Size(93, 21);
+            this.lbngaysinh.TabIndex = 0;
+            this.lbngaysinh.Text = "Ngày Sinh";
+            // 
+            // lbnoisinh
+            // 
+            this.lbnoisinh.AutoSize = true;
+            this.lbnoisinh.Location = new System.Drawing.Point(27, 245);
+            this.lbnoisinh.Name = "lbnoisinh";
+            this.lbnoisinh.Size = new System.Drawing.Size(80, 21);
+            this.lbnoisinh.TabIndex = 0;
+            this.lbnoisinh.Text = "Nơi Sinh";
+            // 
+            // txtngaysinh
+            // 
+            this.txtngaysinh.Location = new System.Drawing.Point(145, 186);
+            this.txtngaysinh.Name = "txtngaysinh";
+            this.txtngaysinh.Size = new System.Drawing.Size(315, 28);
+            this.txtngaysinh.TabIndex = 1;
+            // 
+            // txtnoisinh
+            // 
+            this.txtnoisinh.Location = new System.Drawing.Point(145, 238);
+            this.txtnoisinh.Name = "txtnoisinh";
+            this.txtnoisinh.Size = new System.Drawing.Size(315, 28);
+            this.txtnoisinh.TabIndex = 1;
+            // 
+            // cmbkhoa
+            // 
+            this.cmbkhoa.FormattingEnabled = true;
+            this.cmbkhoa.Location = new System.Drawing.Point(143, 299);
+            this.cmbkhoa.Name = "cmbkhoa";
+            this.cmbkhoa.Size = new System.Drawing.Size(317, 29);
+            this.cmbkhoa.TabIndex = 3;
+            // 
+            // lbkhoa
+            // 
+            this.lbkhoa.AutoSize = true;
+            this.lbkhoa.Location = new System.Drawing.Point(27, 306);
+            this.lbkhoa.Name = "lbkhoa";
+            this.lbkhoa.Size = new System.Drawing.Size(52, 21);
+            this.lbkhoa.TabIndex = 0;
+            this.lbkhoa.Text = "Khoa";
+            // 
+            // lbhocbong
+            // 
+            this.lbhocbong.AutoSize = true;
+            this.lbhocbong.Location = new System.Drawing.Point(27, 361);
+            this.lbhocbong.Name = "lbhocbong";
+            this.lbhocbong.Size = new System.Drawing.Size(89, 21);
+            this.lbhocbong.TabIndex = 0;
+            this.lbhocbong.Text = "Học Bổng";
+            // 
+            // lbtongdiem
+            // 
+            this.lbtongdiem.AutoSize = true;
+            this.lbtongdiem.Location = new System.Drawing.Point(27, 417);
+            this.lbtongdiem.Name = "lbtongdiem";
+            this.lbtongdiem.Size = new System.Drawing.Size(98, 21);
+            this.lbtongdiem.TabIndex = 0;
+            this.lbtongdiem.Text = "Tổng Điểm";
+            // 
+            // txthocbong
+            // 
+            this.txthocbong.Location = new System.Drawing.Point(145, 356);
+            this.txthocbong.Name = "txthocbong";
+            this.txthocbong.Size = new System.Drawing.Size(315, 28);
+            this.txthocbong.TabIndex = 1;
+            // 
+            // txttongdiem
+            // 
+            this.txttongdiem.Location = new System.Drawing.Point(145, 410);
+            this.txttongdiem.Name = "txttongdiem";
+            this.txttongdiem.Size = new System.Drawing.Size(315, 28);
+            this.txttongdiem.TabIndex = 1;
+            // 
+            // btndau
+            // 
+            this.btndau.Location = new System.Drawing.Point(139, 478);
+            this.btndau.Name = "btndau";
+            this.btndau.Size = new System.Drawing.Size(75, 31);
+            this.btndau.TabIndex = 4;
+            this.btndau.Text = "Đầu";
+            this.btndau.UseVisualStyleBackColor = true;
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(139, 515);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 29);
+            this.btnthem.TabIndex = 5;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            // 
+            // txttruoc
+            // 
+            this.txttruoc.Location = new System.Drawing.Point(220, 478);
+            this.txttruoc.Name = "txttruoc";
+            this.txttruoc.Size = new System.Drawing.Size(75, 31);
+            this.txttruoc.TabIndex = 4;
+            this.txttruoc.Text = "Trước";
+            this.txttruoc.UseVisualStyleBackColor = true;
+            // 
+            // btnhuy
+            // 
+            this.btnhuy.Location = new System.Drawing.Point(220, 515);
+            this.btnhuy.Name = "btnhuy";
+            this.btnhuy.Size = new System.Drawing.Size(75, 29);
+            this.btnhuy.TabIndex = 5;
+            this.btnhuy.Text = "Hủy";
+            this.btnhuy.UseVisualStyleBackColor = true;
+            // 
+            // btnsau
+            // 
+            this.btnsau.Location = new System.Drawing.Point(301, 478);
+            this.btnsau.Name = "btnsau";
+            this.btnsau.Size = new System.Drawing.Size(75, 31);
+            this.btnsau.TabIndex = 4;
+            this.btnsau.Text = "Sau";
+            this.btnsau.UseVisualStyleBackColor = true;
+            // 
+            // btnghi
+            // 
+            this.btnghi.Location = new System.Drawing.Point(301, 515);
+            this.btnghi.Name = "btnghi";
+            this.btnghi.Size = new System.Drawing.Size(75, 29);
+            this.btnghi.TabIndex = 5;
+            this.btnghi.Text = "Ghi";
+            this.btnghi.UseVisualStyleBackColor = true;
+            // 
+            // btncuoi
+            // 
+            this.btncuoi.Location = new System.Drawing.Point(385, 478);
+            this.btncuoi.Name = "btncuoi";
+            this.btncuoi.Size = new System.Drawing.Size(75, 31);
+            this.btncuoi.TabIndex = 4;
+            this.btncuoi.Text = "Cuối";
+            this.btncuoi.UseVisualStyleBackColor = true;
+            // 
+            // btnkhong
+            // 
+            this.btnkhong.Location = new System.Drawing.Point(385, 515);
+            this.btnkhong.Name = "btnkhong";
+            this.btnkhong.Size = new System.Drawing.Size(75, 29);
+            this.btnkhong.TabIndex = 5;
+            this.btnkhong.Text = "Không";
+            this.btnkhong.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 591);
+            this.Controls.Add(this.btnkhong);
+            this.Controls.Add(this.btnghi);
+            this.Controls.Add(this.btnhuy);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.btncuoi);
+            this.Controls.Add(this.btnsau);
+            this.Controls.Add(this.txttruoc);
+            this.Controls.Add(this.btndau);
+            this.Controls.Add(this.cmbkhoa);
+            this.Controls.Add(this.ckbphai);
+            this.Controls.Add(this.txttensv);
+            this.Controls.Add(this.txttongdiem);
+            this.Controls.Add(this.txtnoisinh);
+            this.Controls.Add(this.txthosv);
+            this.Controls.Add(this.txthocbong);
+            this.Controls.Add(this.txtngaysinh);
+            this.Controls.Add(this.txtmasv);
+            this.Controls.Add(this.lbkhoa);
+            this.Controls.Add(this.lbtongdiem);
+            this.Controls.Add(this.lbnoisinh);
+            this.Controls.Add(this.lbhocbong);
+            this.Controls.Add(this.lbphai);
+            this.Controls.Add(this.lbngaysinh);
+            this.Controls.Add(this.lbhoten);
+            this.Controls.Add(this.lbmasv);
+            this.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Quản Lý Sinh Viên";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbmasv;
+        private System.Windows.Forms.TextBox txtmasv;
+        private System.Windows.Forms.Label lbhoten;
+        private System.Windows.Forms.TextBox txthosv;
+        private System.Windows.Forms.TextBox txttensv;
+        private System.Windows.Forms.CheckBox ckbphai;
+        private System.Windows.Forms.Label lbphai;
+        private System.Windows.Forms.Label lbngaysinh;
+        private System.Windows.Forms.Label lbnoisinh;
+        private System.Windows.Forms.TextBox txtngaysinh;
+        private System.Windows.Forms.TextBox txtnoisinh;
+        private System.Windows.Forms.ComboBox cmbkhoa;
+        private System.Windows.Forms.Label lbkhoa;
+        private System.Windows.Forms.Label lbhocbong;
+        private System.Windows.Forms.Label lbtongdiem;
+        private System.Windows.Forms.TextBox txthocbong;
+        private System.Windows.Forms.TextBox txttongdiem;
+        private System.Windows.Forms.Button btndau;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button txttruoc;
+        private System.Windows.Forms.Button btnhuy;
+        private System.Windows.Forms.Button btnsau;
+        private System.Windows.Forms.Button btnghi;
+        private System.Windows.Forms.Button btncuoi;
+        private System.Windows.Forms.Button btnkhong;
+    }
+}
+
---
+++ CSDL/Chuong_1/BT_01/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BT_01
+{
+    public partial class Form1 : Form
+    {
+        // Khai báo các đối tượng 
+        // 1. Khai báo một biến (đối tượng) DataSet
+        DataSet ds = new DataSet();
+        // 2. Khai báo các DataTable tương ứng với các bảng có chứa dữ liệu
+        DataTable tblKhoa = new DataTable("KHOA");
+        DataTable tblSinhVien = new DataTable("SINHVIEN");
+        DataTable tblKetQua = new DataTable("KETQUA");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Tạo bảng cấu trúc các bảng
+            Tao_Cau_Truc_Cac_Bang();
+            Moc_Noi_Quan_He_Cac_Bang();
+            Nhap_Lieu_Cho_Cac_Bang();
+        }
+
+        private void Nhap_Lieu_Cho_Cac_Bang()
+        {
+            Nhap_Lieu_tblKhoa();
+            Nhap_Lieu_tblSinhVien();
+            Nhap_Lieu_tblKetQua();
+        }
+
+        private void Nhap_Lieu_tblKetQua()
+        {
+            // Nhap lieu cho tblKhoa => Doc du lieu tu tap tin KETQUA.txt
+            string[] Mang_KQ = File.ReadAllLines(@"..\..\..\data\KETQUA.txt");
+            foreach (string Chuoi_KQ in Mang_KQ)
+            {
+                // Tach chuoi khoa thanh cac thanh phan tuong ung voi cac cot tblKetQua
+                string[] Mang_Thanh_Phan = Chuoi_KQ.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                // Tao 1 dong moi co cau truc giong cau truc cua 1 dong trong tblKetQua
+                DataRow rkq = tblKetQua.NewRow();
+                // Gan gia tri cho cac cot cua dong moi tao ra
+                // Cach 1:
+                //rkh[0] = Mang_Thanh_Phan[0];
+                //rkh[1] = Mang_Thanh_Phan[1];
+                // Cach 2:
+                for (int i = 0; i < Mang_Thanh_Phan.Length; i++)
+                {
+                    rkq[i] = Mang_Thanh_Phan[i];
+                }
+
+
+                // Them dong vua tao vao tblSinhVien
+                tblKetQua.Rows.Add(rkq);
+            }
+        }
+
+        private void Nhap_Lieu_tblSinhVien()
+        {
+            // Nhap lieu cho tblKhoa => Doc du lieu tu tap tin SINHVIEN.txt
+            string[] Mang_SV = File.ReadAllLines(@"..\..\..\data\SINHVIEN.txt");
+            foreach (string Chuoi_SV in Mang_SV)
+            {
+                // Tach chuoi khoa thanh cac thanh phan tuong ung voi cac cot tblSinhVien
+                string[] Mang_Thanh_Phan = Chuoi_SV.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
+                // Tao 1 dong moi co cau truc giong cau truc cua 1 dong trong tblSinhVien
+                DataRow rsv = tblSinhVien.NewRow();
+                // Gan gia tri cho cac cot cua dong moi tao ra
+                // Cach 1:
+                //rkh[0] = Mang_Thanh_Phan[0];
+                //rkh[1] = Mang_Thanh_Phan[1];
+                // Cach 2:
+                for (int i = 0; i < Mang_Thanh_Phan.Length; i++)
+                {
+                    rsv[i] = Mang_Thanh_Phan[i];
+                }
+
+
+                // Them dong vua tao vao tblSinhVien
+                tblSinhVien.Rows.Add(rsv);
+            }
+
+        }
+            private void Nhap_Lieu_tblKhoa()
+            {
+                // Nhap lieu cho tblKhoa => Doc du lieu tu tap tin KHOA.txt
+                string[] Mang_Khoa = File.ReadAllLines(@"..\..\..\data\KHOA.txt");
+                foreach (string Chuoi_Khoa in Mang_Khoa)
+                {
+                    // Tach chuoi khoa thanh cac thanh phan tuong ung voi MaKH va TenKH
+                    string[] Mang_Thanh_Phan = Chuoi_Khoa.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                    // Tao 1 dong moi co cau truc giong cau truc cua 1 dong trong tblKhoa
+                    DataRow rkh = tblKhoa.NewRow();
+                    // Gan gia tri cho cac cot cua dong moi tao ra
+                    rkh[0] = Mang_Thanh_Phan[0];
+                    rkh[1] = Mang_Thanh_Phan[1];
+
+
+                    // Them dong vua tao vao tblKhoa
+                    tblKhoa.Rows.Add(rkh);
+
+                }
+            }
+
+            private void Moc_Noi_Quan_He_Cac_Bang()
+            {
+                // Tao quan he giua tblkhoa va tblsinhvien
+                ds.Relations.Add("FK_KH_SV", ds.Tables["KHOA"].Columns["MaKH"], ds.Tables["SINHVIEN"].Columns["MaKH"], true);
+
+                // Tao quan he giua tblsinhvien va tnlketqua
+                ds.Relations.Add("FK_SV_KQ", ds.Tables["SINHVIEN"].Columns["MaSV"], ds.Tables["KETQUA"].Columns["MaSV"], true);
+
+                // Loai bo Cascade delete trong cac quan he
+                ds.Relations["FK_KH_SV"].ChildKeyConstraint.DeleteRule = Rule.None;
+                ds.Relations["FK_SV_KQ"].ChildKeyConstraint.DeleteRule = Rule.None;
+            }
+
+            private void Tao_Cau_Truc_Cac_Bang()
+            {
+                // Tao cau truc cho DataTable tuong ung voi bang KHOA
+                tblKhoa.Columns.Add("MaKH", typeof(string));
+                tblKhoa.Columns.Add("TenKH", typeof(string));
+                // Tao khoa chinh cho tblKhoa
+                tblKhoa.PrimaryKey = new DataColumn[] { tblKhoa.Columns["MaKH"] };
+                // Tạo cấu trúc cho DataTable tương ứng với bảng SINHVIEN
+                tblSinhVien.Columns.Add("MaSV", typeof(string));
+                tblSinhVien.Columns.Add("HoSV", typeof(string));
+                tblSinhVien.Columns.Add("TenSV", typeof(string));
+                tblSinhVien.Columns.Add("Phai", typeof(Boolean));
+                tblSinhVien.Columns.Add("NgaySinh", typeof(DateTime));
+                tblSinhVien.Columns.Add("NoiSinh", typeof(string));
+                tblSinhVien.Columns.Add("MaKH", typeof(string));
+                tblSinhVien.Columns.Add("HocBong", typeof(double));
+                // Tao khoa chinh cho tblSinhVien
+                tblSinhVien.PrimaryKey = new DataColumn[] { tblSinhVien.Columns["MaSV"] };
+
+                // Tạo cấu trúc cho DataTable tương ứng với bảng KETQUA
+                tblKetQua.Columns.Add("MaSV", typeof(string));
+                tblKetQua.Columns.Add("MaMH", typeof(string));
+                tblKetQua.Columns.Add("Diem", typeof(Single));
+                // Tao Khoa chinh cho KETQUA
+                tblKetQua.PrimaryKey = new DataColumn[] { tblKetQua.Columns["MaSV"], tblKetQua.Columns["MaMH"] };
+
+
+                // Them cac DataTable cho DataSet
+                //ds.Tables.Add(tblKhoa);
+                //ds.Tables.Add(tblSinhVien);
+                //ds.Tables.Add(tblKetQua);
+
+                // Them dong thoi nhieu DataTable vao dataset
+                ds.Tables.AddRange(new DataTable[] { tblKhoa, tblSinhVien, tblKetQua });
+
+            }
+        } 
+}
+// Thành phần DataTable
+// 1. Dùng để lưu trữ dữ liệu của một Table trong bộ nhớ
+// 2. Tạo một đối tượng DataTable: New DataTable ("<Tên DataTable>");
+// 3. Tạo ra các cột (DataColumn): <Biến DataTable>.Column.add("<Tên Cột>", <Kểu dữ liệu>);
+// 4. Tạo khóa chính cho DataTable => PrimaryKey => Là mảng các DataColumn
+// 5. Thêm các DataTable và DataSet
+// 6. Móc nối quan hệ giữa các DataTable
